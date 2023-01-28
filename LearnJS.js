f8bc4aa6b18a26945328c56608e3ded626b331e6
Lumine3449 <>
2023-01-28 12:36:31 -0700

    JS entries updated and added

---
+++ LearnJS.js
@@ -0,0 +1,16 @@
+// in-line comments are lines of text that JavaScript will completely ignore.
+/* Multi-line comments
+Multi-line comments are similar to in-line comments,
+but they can hold more that one line of text.
+*/
+
+
+
+// Data Types and variables: undefined, null, boolean, string, symbol, number, and object
+// Undefined is something that hasn't been defined yet. So you may have a variable that you haven't set to anything yet.
+// Null means nothing, so you've set something to be something, and that something is nothing.
+// Boolean means true or false.
+// String. The string object is used to represent and manipulate a sequence of characters. Strings are useful for holding data that can be represented in text form. Some of the most-used operations on strings are to check their length, to build and concatenate them using the + and += string operators, checking for the existence or location of substrings with the indexOf() method, or extracting substrings with the substring() method.
+// Symbol
+// Number
+// Object 
