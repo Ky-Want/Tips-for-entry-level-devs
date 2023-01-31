e278857784e425b436bdea938e5a0eb6fa0531a2
Lumine3449 <>
2023-01-31 16:20:41 -0700

    Entry added

--- LearnJS.js
+++ LearnJS.js
@@ -13,4 +13,4 @@ but they can hold more that one line of text.
 // String. The string object is used to represent and manipulate a sequence of characters. Strings are useful for holding data that can be represented in text form. Some of the most-used operations on strings are to check their length, to build and concatenate them using the + and += string operators, checking for the existence or location of substrings with the indexOf() method, or extracting substrings with the substring() method.
 // Symbol
 // Number
-// Object 
+// Object
